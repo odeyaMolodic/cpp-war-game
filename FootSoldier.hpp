36a6b1be7987ce3e1a110eceab15974a5b5355a8
odeyaMolodic <57504045+odeyaMolodic@users.noreply.github.com>
2020-06-04 21:27:19 +0300

    empty constructor

--- FootSoldier.hpp
+++ FootSoldier.hpp
@@ -12,6 +12,7 @@ class FootSoldier: public Soldier
 public:
     FootSoldier(uint num, int hp=100, int max=100, int damage=10, string type="FootSoldier") : Soldier(num, hp, max, damage, type) {}
     FootSoldier(Soldier* s): Soldier(s) {}
+    FootSoldier();
     void attack(vector<vector<Soldier*>> &b, pair<int,int> location);
     double distance(int x1, int y1, int x2, int y2) // d = sqrt((x1-x2)^2 + (y1-y2)^2)
     {
